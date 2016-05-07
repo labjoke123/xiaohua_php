a2c430d432649b140c4f0e888ea08b96d017597e
zhouhonggang <zhouhonggang@autohome.com.cn>
2016-05-07 13:29:19 +0800

    add manuscript frontend

---
+++ frontend/controllers/ManuscriptController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace frontend\controllers;
+
+class ManuscriptController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
