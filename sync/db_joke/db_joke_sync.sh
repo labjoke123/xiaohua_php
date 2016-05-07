87c1efeb4fb8ef399e13b04561cddd6901bc2095
zhouhonggang <zhouhonggang@autohome.com.cn>
2016-05-07 16:43:35 +0800

    create sync sh

--- sync/db_joke/db_joke_sync.sh
+++ sync/db_joke/db_joke_sync.sh
@@ -1,2 +1,16 @@
 #! /bin/sh
 
+now=`date +%Y%m%d%H%M`
+file_sql="db_joke_struct_${now}.sql"
+
+# dump local sql file
+# mysqldump -uroot -p123456 db_joke > ${file_sql}
+mysqldump --opt -d -uroot -p123456 db_joke > ${file_sql}
+
+# upload to 112.74.78.105
+host=112.74.78.105
+scp ${file_sql} root@${host}:/data/sync/db_joke/
+
+
+
+
