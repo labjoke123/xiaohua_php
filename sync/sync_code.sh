e1b6ea6e8db0a6d84f519db69c7d1fe4d530c57b
zhouhonggang <zhouhonggang@autohome.com.cn>
2016-05-07 17:39:04 +0800

    modify sync code

--- sync/sync_code.sh
+++ sync/sync_code.sh
@@ -7,11 +7,11 @@ mkdir -p ${folder}
 
 # go to project git folder
 cd /Users/gangzi/htdocs/xiaohua
-git archive --format=zip --output="${folder}/xiaohua.zip" online
+git archive --format=zip --output="${folder}/xiaohua.zip" master
 
 # go to publish folder and rsync
 cd ${folder}
 unzip xiaohua.zip
 rm xiaohua.zip
 
-rsync -v -r ${folder} root@112.74.78.105:/var/www/html
+#rsync -rv --exclude-from="/Users/gangzi/Work/publish_xiaohua/exclude" ${folder} root@112.74.78.105:/var/www/html
