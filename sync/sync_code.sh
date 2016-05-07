2f3c6c10a63306226121683694f8e27cf2297844
zhouhonggang <zhouhonggang@autohome.com.cn>
2016-05-07 18:19:01 +0800

    modify sync master

--- sync/sync_code.sh
+++ sync/sync_code.sh
@@ -2,16 +2,30 @@
 now=`date +%Y%m%d%H%M`
 
 # create publish folder
-folder="/Users/gangzi/Work/publish_xiaohua/${now}/xiaohua"
-mkdir -p ${folder}
+p_folder="/Users/gangzi/Work/publish_xiaohua"
+xh_folder="${p_folder}/xiaohua"
+bk_folder="${p_folder}/back_${now}"
+
+if [ -d ${xh_folder} ]; then
+	echo "cp -R ${xh_folder} ${bk_folder}"
+	cp -R ${xh_folder} ${bk_folder}
+	rm -rf ${xh_folder}
+fi
+
+mkdir -p ${xh_folder}
 
 # go to project git folder
 cd /Users/gangzi/htdocs/xiaohua
-git archive --format=zip --output="${folder}/xiaohua.zip" master
+git tag xiaohua_${now}
+git archive --format=zip --output="${xh_folder}/xiaohua.zip" master
 
 # go to publish folder and rsync
-cd ${folder}
-unzip xiaohua.zip
+cd ${xh_folder}
+echo "unzip xiao.zip"
+unzip -q xiaohua.zip
 rm xiaohua.zip
 
-#rsync -rv --exclude-from="/Users/gangzi/Work/publish_xiaohua/exclude" ${folder} root@112.74.78.105:/var/www/html
+# rsync xiaohua project
+ex_file="/Users/gangzi/Work/publish_xiaohua/exclude"
+rsync -r --exclude-from=${ex_file} ${xh_folder} root@112.74.78.105:/var/www/html
+echo "finish rsync"
