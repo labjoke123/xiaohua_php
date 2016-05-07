97824bb192c2f36730bb97177408261bfb41edac
zhouhonggang <zhouhonggang@autohome.com.cn>
2016-05-07 13:40:23 +0800

    create Manuscript controller frontend

--- frontend/controllers/ManuscriptController.php
+++ frontend/controllers/ManuscriptController.php
@@ -2,11 +2,60 @@
 
 namespace frontend\controllers;
 
-class ManuscriptController extends \yii\web\Controller
+use frontend\models\Manuscript;
+
+class ManuscriptController extends \frontend\controllers\FrontController
 {
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public function actionList()
+    {
+    	$manuscripts = Manuscript::find()->all();
+
+    	$state = array(
+    		'stateCode'=>'200',
+    		'stateMessage'=>'OK'
+    	);
+
+    	$data = array(
+    		'total'=>'100',
+    		'pageNum'=>'1',
+    		'pageSize'=>'20',
+    		'list'=>array()
+    	);
+
+    	foreach ($manuscripts as $manuscript) {
+    		$data['list'][] = $manuscript->attributes;
+    	}
+
+    	$resArray = array(
+    		'state'=>$state,
+    		'data'=>$data
+    	);
+
+    	$this->outputDataFormat($resArray);
+    }
+
+    public function actionDetail($id)
+    {
+    	$manuscript = Manuscript::find()->where(['id'=>$id])->one();
+
+		$state = array(
+    		'stateCode'=>'200',
+    		'stateMessage'=>'OK'
+    	);
+
+    	$data = $manuscript->attributes;
+
+    	$resArray = array(
+    		'state'=>$state,
+    		'data'=>$data
+    	);
+
+    	$this->outputDataFormat($resArray);
     }
 
 }
