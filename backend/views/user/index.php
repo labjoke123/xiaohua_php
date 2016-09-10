18bca12b2d1efa7ff1eb61c173122534e416511e
zhouhonggang <zhouhonggang@autohome.com.cn>
2016-09-10 08:26:10 +0800

    generate crud for User

---
+++ backend/views/user/index.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'user_id',
+            'user_sn',
+            'is_del',
+            'create_time:datetime',
+            'update_time:datetime',
+            // 'user_name',
+            // 'email:email',
+            // 'age',
+            // 'gender',
+            // 'portrait',
+            // 'address:ntext',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
